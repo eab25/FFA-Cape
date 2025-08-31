55806ee4e142c2891c0a10d556e3c0a82d295996
eab25 <130369043+eab25@users.noreply.github.com>
2025-08-31 14:25:23 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,29 +2,5 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Title for your app
-st.title("Baltic Exchange Historic Data Visualization")
-
-# Upload the CSV file
-uploaded_file = st.file_uploader("Upload Baltic Exchange CSV", type="csv")
-
-if uploaded_file:
-    df = pd.read_csv(uploaded_file, parse_dates=['Date'])
-    st.write("Data Preview:", df.head())
-
-    # Select columns to plot
-    options = st.multiselect(
-        "Select columns to plot",
-        ['C5TC', 'HS7TC', 'P4TC', 'S10TC'],
-        default=['C5TC', 'HS7TC', 'P4TC', 'S10TC']
-    )
-
-    # Plot using matplotlib
-    fig, ax = plt.subplots(figsize=(18, 8))
-    for col in options:
-        ax.plot(df['Date'], df[col], label=col)
-    ax.set_xlabel('Date')
-    ax.set_ylabel('Value')
-    ax.set_title('Baltic Exchange Historic Rates')
-    ax.legend()
-    st.pyplot(fig)
+st.write("hello')
+         

4b0e4bd0f5d7a1c63f4722e5ee0f779947c88d6e
eab25 <130369043+eab25@users.noreply.github.com>
2025-08-31 14:39:22 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
-st.write("hello")       
+st.title("FFA Trading")
+
+uploaded_file = st.file_uploader("Choosen File", type="csv")
+
+if uploaded_file is not None:
+  st.write("File uploaded...")
+  
+
